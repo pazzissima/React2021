9b4df32de55c32dbfbc264930bfac758e472a216
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-23 20:53:15 -0700

    setting up Redux for the first time, yay!

---
+++ redux-demo.js
@@ -0,0 +1,18 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0}, action) => {
+	return {
+		counter: state.counter + 1
+	};
+};
+
+//Here we just point to counterReducer, not execute it. It will be executed by Reduc
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+	const latestState = store.getState();
+	console.log(latestState);
+};
+
+//Here we just point to counterSubscriber, not execute it. It will be executed by Reduc
+store.subscribe(counterSubscriber);
