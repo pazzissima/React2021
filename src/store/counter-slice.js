9b695b314681b13f2f571ace8f30ab75a6e71a7d
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-25 22:27:26 -0700

    splitting the code

---
+++ src/store/counter-slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+	name: 'counter',
+	initialState: initialCounterState,
+	reducers: {
+		//These are unique action identifiers
+		increment(state) {
+			//!!! Redux toolkit uses a package that will detect cases like this and 
+			//clone state instead of changing the existing one
+			//This is mutable-looking way but not mutable
+			state.counter++;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		increase(state, action) {
+			//payload is the name of a variable that will hold any extra data you might be dispatching
+			state.counter = state.counter + action.payload;
+		},
+		toggleCounter(state) {
+			state.showCounter = !state.showCounter;
+		}
+	}
+});
+
+export const counterActions = counterSlice.actions;
+
+export default counterSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,58 +1,14 @@
-import {createSlice, configureStore} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-	name: 'counter',
-	initialState: initialCounterState,
-	reducers: {
-		//These are unique action identifiers
-		increment(state) {
-			//!!! Redux toolkit uses a package that will detect cases like this and 
-			//clone state instead of changing the existing one
-			//This is mutable-looking way but not mutable
-			state.counter++;
-		},
-		decrement(state) {
-			state.counter--;
-		},
-		increase(state, action) {
-			//payload is the name of a variable that will hold any extra data you might be dispatching
-			state.counter = state.counter + action.payload;
-		},
-		toggleCounter(state) {
-			state.showCounter = !state.showCounter;
-		}
-	}
-});
-
-
-const initialAuthState = {
-	isAuthenticated: false
-}
-
-const authSlice = createSlice({
-	name: 'auth',
-	initialState:  initialAuthState,
-	reducers: {
-		login(state) {
-			state.isAuthenticated = true;
-		},
-		logout(state) {
-			state.isAuthenticated = false;
-		}
-	}
-})
+import counterReducer from './counter-slice';
+import authReducer from './auth-slice';
 
 const store = configureStore({
 	reducer: {
-		counter: counterSlice.reducer,
-		auth: authSlice.reducer
+		counter: counterReducer,
+		auth: authReducer
 	}
 })
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
 
