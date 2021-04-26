1507f207955815de2af2476d1464e4f9b0b3a427
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-25 19:55:31 -0700

    added logoutHandler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
 
 import classes from './Header.module.css';
 
 const Header = () => {
   const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const logoutHandler = (event) =>{
+    event.preventDefault();
+    dispatch(authActions.logout());
+  }
 
   return (
     <header className={classes.header}>
@@ -17,7 +24,7 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>}
