af328a13c12a3b7b04194f3df4d8e05848e372e6
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-04-30 19:59:21 -0700

    updated cart but it combines both items together. Whyyyyyyyyyyy?

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -9,17 +9,17 @@ const cartSlice = createSlice({
 	reducers: {
 		addItemToCart(state, action) {
 			const newItem = action.payload;
-			const existingItem = state.items.find(item => item.id ===newItem.id);
+			const existingItem = state.items.find(item => item.id === newItem.id);
 			state.totalQuantity++;
 			if (!existingItem) {
 				state.items.push({
-					itemId: newItem.id, 
+					id: newItem.id, 
 					price: newItem.price, 
 					quantity: 1, 
 					totalPrice: newItem.price,
 					name: newItem.title
 				});
-			} else{
+			} else {
 				existingItem.quantity++;
 				existingItem.totalPrice = existingItem.totalPrice + newItem.price;
 			}
