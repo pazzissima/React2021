6c096cc43447f4618f6e309b4da8dc3de3c7d106
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-05-03 16:25:41 -0700

    keeping data transformation inside a reducer but using a component to send HTTP request after store updates

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,17 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  //ORDER: First - store updates. Then we select updated store to send a request
+  useEffect(() => {
+  	//override existing cart with new data
+  	fetch('https://some_firebase_api_url/cart.json', { 
+	  	method: 'PUT',
+	  	body: JSON.stringify(cart),
+	});
+	// this function re-executes whenever our cart changes. [cart] - dependency
+  }, [cart]);
 
   return (
     <Layout>
