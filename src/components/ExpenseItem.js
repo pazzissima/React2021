e592ba7b7a3ae5c709619aa9f3b33bbb89fec8d2
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-05-22 10:34:10 -0700

    splitting the components, passing props down the tree

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,12 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-	const month = props.dt.toLocaleString('en-US', {month: 'long'});
-	const day = props.dt.toLocaleString('en-US', {day: '2-digit'});
-	const year = props.dt.getFullYear();
+	
 
 	return (
 		<div className="expense-item">
-			<div>
-				<div>{month}</div>
-				<div>{year}</div>
-				<div>{day}</div>
-			</div>
+		<ExpenseDate dat={props.dt}/>
 			<div className="expense-item__description">
 				<h2>{props.exp}</h2>
 				<div className="expense-item__price">${props.amt}</div>
