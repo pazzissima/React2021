7e88f88ad8d4f0d888f7afc549ded8ae27fb5ba1
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-05-22 10:16:09 -0700

    formatting date props

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
   		id: 'e2',
   		title: 'New TV',
   		amount: 599.99,
-  		date: new Date(2021, 5, 22),
+  		date: new Date(2021, 4, 22),
   	},
   	{
   		id: 'e3',
   		title: 'Rent',
   		amount: 1700,
-  		date: new Date(2021, 5, 22),
+  		date: new Date(2021, 7, 22),
   	}
   ];
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+	const month = props.dt.toLocaleString('en-US', {month: 'long'});
+	const day = props.dt.toLocaleString('en-US', {day: '2-digit'});
+	const year = props.dt.getFullYear();
 
 	return (
 		<div className="expense-item">
-			<div>{props.dt.toISOString()}</div>
+			<div>
+				<div>{month}</div>
+				<div>{year}</div>
+				<div>{day}</div>
+			</div>
 			<div className="expense-item__description">
 				<h2>{props.exp}</h2>
 				<div className="expense-item__price">${props.amt}</div>
