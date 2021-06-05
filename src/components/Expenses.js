4038f5fd2c0b3aadb450bcfab688c643ab90c904
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-06-04 23:03:36 -0700

    extract duplicate CSS code into a wrapper component - for Expenses too

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from './Card';
 
 function Expenses(props) {
 
 	return (
-		<div className="expenses">
+		<Card className="expenses">
 		  <ExpenseItem exp={props.items[0].title} amt={props.items[0].amount} dt={props.items[0].date}></ExpenseItem>
 	      <ExpenseItem exp={props.items[1].title} amt={props.items[1].amount} dt={props.items[1].date}></ExpenseItem>
 	      <ExpenseItem exp={props.items[2].title} amt={props.items[2].amount} dt={props.items[2].date}></ExpenseItem>
-		</div>
+		</Card>
 	)
 
 }
