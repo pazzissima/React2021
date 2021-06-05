a3bebfe01e9af96a761865d6d8597f27afbf2591
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-06-04 22:58:35 -0700

    wrapper component and children props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) {
 	
 
 	return (
-		<div className="expense-item">
-		<ExpenseDate dat={props.dt}/>
+		<Card className="expense-item">
+			<ExpenseDate dat={props.dt}/>
 			<div className="expense-item__description">
 				<h2>{props.exp}</h2>
 				<div className="expense-item__price">${props.amt}</div>
 			</div>
-		</div>
+		</Card>
 	)
 }
 
