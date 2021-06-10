1e3b28b46bc6033d83fba67c80707f12738f4c5d
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-06-09 23:09:12 -0700

    setState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+
+	const [title, setTitle] = useState(props.exp);
+	// useState always returns an array with 2 elements - first one is the current state value, 
+	// second is a f-n for updating it
+
+
 	const clickHandler = () => {
-		console.log('Clicked!!!');
+		setTitle('Updated!');
+		console.log(title);
 	}
 
 	return (
 		<Card className="expense-item">
 			<ExpenseDate dat={props.dt}/>
 			<div className="expense-item__description">
-				<h2>{props.exp}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amt}</div>
 			</div>
 			<button onClick={clickHandler}>Change title</button>
