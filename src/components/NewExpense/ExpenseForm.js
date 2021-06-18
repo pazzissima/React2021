853cf8ceee957091500a5a8fadce56348a285def
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2021-06-17 20:43:47 -0700

    2-way binding - allows to gather using input and also change it upon form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -27,7 +27,11 @@ const ExpenseForm = () => {
 			amount: enteredAmount,
 			date: new Date(enteredDate)
 		}
-		console.log('expenseDate: '+expenseData)
+		
+		console.log(expenseData);
+		setEnteredTitle('');
+		setEnteredAmount('');
+		setEnteredDate('');
 	};
 
 	return (
@@ -35,17 +39,17 @@ const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input type="text" value={enteredTitle} onChange={titleChangeHandler} />
 				</div>
 
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+					<input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler}/>
 				</div>
 
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+					<input type="date" value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
 				</div>
 			</div>
 			<div className="new-expense__actions">
